0490fe08155d00aede2f92c0d459991ba79cd001
XcelTec-060 <jenilsatani97809@gmail.com>
2022-03-29 15:04:49 +0530

    react

--- src/Userlist.js
+++ src/Userlist.js
@@ -61,29 +61,7 @@ function Copy() {
 
 
             <div>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">Medicine Management Task</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="Profile">Logout</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="Userlist">User List</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="Mlist">Medicine List</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="Profile">My-Profile</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+             
                 <div>
                     <MaterialTable title=" User List"
 
